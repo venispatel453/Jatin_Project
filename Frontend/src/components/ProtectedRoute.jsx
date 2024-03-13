7866033fe0b7867e0ed5e76872135a570a16b4f9
jatin <jatinramchandani15@gmail.com>
2024-03-13 23:22:46 +0530

    added logic to update context

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -1,16 +1,39 @@
 // ProtectedRoute.js
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Route, Navigate, Outlet } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import AuthContext from "../context/AuthProvider";
+import { updateRoleOfUser } from "../util/users";
 
 const ProtectedRoute = ({ element, ...rest }) => {
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { auth, setAuth } = useContext(AuthContext);
+  const { isAuthenticated, isLoading, user } = useAuth0();
 
-  if (isLoading) {
+  const setUserRole = async () => {
+    try {
+      const res = await updateRoleOfUser(user.sub);
+      console.log(res);
+      setAuth({ id: user.sub, role: res[0].name });
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      if (!isAuthenticated) return;
+      setUserRole();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [isAuthenticated]);
+
+  if (isLoading || !auth.role) {
     return <div>Loading...</div>; // Handle loading state if needed
   }
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  return isAuthenticated && auth?.role ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
