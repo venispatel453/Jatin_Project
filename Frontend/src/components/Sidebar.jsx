db833bbda1ccade538a970352ac8d056562d9a40
jatin <jatinramchandani15@gmail.com>
2024-03-13 23:30:17 +0530

    added conditionsla rendering of components

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react"; // Importing React library
+import React, { useContext } from "react"; // Importing React library
 import "../styling/sidebar.css"; // Importing CSS styles for the sidebar component
 import "monday-ui-react-core/tokens"; // Importing tokens for styling
 import { FaPlus } from "react-icons/fa6";
@@ -10,9 +10,11 @@ import {
   Flex,
   MenuItemButton,
 } from "monday-ui-react-core"; // Importing necessary components from Monday UI React Core library
+import AuthContext from "../context/AuthProvider";
 
 // Sidebar component definition
 const Sidebar = () => {
+  const { auth } = useContext(AuthContext);
   return (
     // Sidebar wrapper div with appropriate class name
     <div className="sidebar-wrapper">
@@ -21,11 +23,18 @@ const Sidebar = () => {
         {/* Flex container to align menu items */}
         <Flex justify="Center" gap={10} direction="Column">
           {/* Menu component for displaying menu items */}
-          <NavLink to="/addProject">
-            <button>add new project</button>
-          </NavLink>
-          <NavLink to="/addUser">
-            <button>Add User</button>
+          {(auth.role === "Admin" || auth.role === "Auditor") && (
+            <NavLink to="/addProject">
+              <button>add new project</button>
+            </NavLink>
+          )}
+          {auth.role === "Admin" && (
+            <NavLink to="/addUser">
+              <button>Add User</button>
+            </NavLink>
+          )}
+          <NavLink to="/">
+            <button>Projects</button>
           </NavLink>
         </Flex>
       </Box>
