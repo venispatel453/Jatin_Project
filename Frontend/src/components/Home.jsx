0825158ef7053259abc3d1b4d7b285685e0e7a93
jatin <jatinramchandani15@gmail.com>
2024-03-13 23:32:43 +0530

    added conditional rendering based on user role

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import NavigationTab from "./NavigationTab";
+import AuthContext from "../context/AuthProvider";
 
 const Home = () => {
   const [associatedProjects, setAssociatedProjects] = useState([]);
   const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const { auth } = useContext(AuthContext);
 
   const fetchData = async () => {
     try {
+      //console.log(`${BASE_URL}/projects`, process.env.REACT_APP_BASE_URL);
       let { data } = await axios.get(`${BASE_URL}/projects`, {
-        params: { id: 1 },
+        params: { id: auth.id },
       });
       console.log(data);
       data = data.data;
