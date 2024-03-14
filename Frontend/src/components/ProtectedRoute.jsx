1d78d8ab07aeb18b10644555406a967f5cbd3516
jatin <jatinramchandani15@gmail.com>
2024-03-15 01:13:49 +0530

    updated logic of setting user role

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -11,22 +11,27 @@ const ProtectedRoute = ({ element, ...rest }) => {
   const [role, setRole] = useState(false);
   const navigate = useNavigate();
 
-  // const setUserRole = async () => {
-  //   try {
-  //     const res = await fetchRoleOfUser(user.sub);
-  //     console.log(res);
-  //     setAuth({ id: user.sub, role: res[0].name });
-  //     console.log(res);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const setUserRole = async () => {
+    try {
+      const res = await fetchRoleOfUser(user.sub);
+      console.log(res);
+      setAuth({ id: user.sub, role: res[0].name });
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  if (isLoading) {
+  useEffect(() => {
+    if (isAuthenticated) setUserRole();
+    else setAuth({});
+  }, [isAuthenticated]);
+
+  if (isLoading || (isAuthenticated && !auth.role)) {
     return <div>Loading...</div>; // Handle loading state if needed
   }
 
-  return isAuthenticated && auth?.role ? <Outlet /> : <Navigate to="/login" />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
